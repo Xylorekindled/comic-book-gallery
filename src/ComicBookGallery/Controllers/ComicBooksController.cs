033dcee5ae432ca7eea24e7aa6cdf7dd4d90221d
XyloRekindled <uprashanth94@outlook.com>
2018-03-12 18:45:02 +0530

    Updated ComicBook details view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,15 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Details()
         {
+            ViewBag.seriesTitle = "The Amazing Spider-Man";
+            ViewBag.issueNumber = 700;
+            ViewBag.description = "<p>Final Issue!! Witness the final hour </p>";
+            ViewBag.artists = new string[]
+            {
+                "Script: Dan Scott",
+                "Inking: Peter Stones"
+            };
+
             return View();
         }
     }
